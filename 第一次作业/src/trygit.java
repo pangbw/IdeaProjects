778aeaf10ef20e9811c80ef06763f641e5d3bfac
pangbw <2761268695@qq.com>
2021-10-24 00:39:46 +0800

    [feather]第一次作业 类里写了个main

--- 第一次作业/src/trygit.java
+++ 第一次作业/src/trygit.java
@@ -1,5 +1,11 @@
 public class trygit {
     public static void main(String[] args){
+        for(int i = 1;i <= 9;i ++){
+            for (int j = 1; j <= i;j++){
+                System.out.print(j+"*"+i+"="+j*i+" ");
+            }
+            System.out.println();
 
     }
+}
 }
