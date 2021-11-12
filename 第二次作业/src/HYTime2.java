52f03d64f7a7161eaaf776fe1bd30083c0f78949
pangbw <2761268695@qq.com>
2021-11-13 00:06:25 +0800

    [feather]第二次作业 初等试验

--- 第二次作业/src/HYTime2.java
+++ 第二次作业/src/HYTime2.java
@@ -112,27 +112,30 @@ class Equipment {
     private int attackPlus;
     private int apPlus;
 
-    public Equipment() {
-
-    }
-
-    public Equipment(String name, int hp, int attack, int ap){
-        Dlsword dlsword = new Dlsword();
-        hp = hp + dlsword.getApPlus();
-        attack = attack + dlsword.getAttackPlus();
-        ap = ap + dlsword.getHpPlus();
-        ename = dlsword.getName();
-    }
-
-    public Equipment(String name, int hpPlus, int attackPlus, int apPlus) {
-        this.setName(name);
-        this.setHpPlus(hpPlus);
-        this.setAttackPlus(attackPlus);
-        this.setApPlus(apPlus);
+    public Equipment(String ename, int ehpPlus, int eattackPlus, int eapPlus) {
         this.getName();
         this.getHpPlus();
         this.getAttackPlus();
         this.getApPlus();
+        this.setName(ename);
+        this.setHpPlus(ehpPlus);
+        this.setAttackPlus(eattackPlus);
+        this.setApPlus(eapPlus);
+
+    }
+    public void equipment(String ename, int ehpPlus, int eattackPlus, int eapPlus) {
+        this.getName();
+        this.getHpPlus();
+        this.getAttackPlus();
+        this.getApPlus();
+        this.setName(ename);
+        this.setHpPlus(ehpPlus);
+        this.setAttackPlus(eattackPlus);
+        this.setApPlus(eapPlus);
+
+    }
+
+    Equipment() {
     }
 
     public void setName(String name) {
@@ -171,6 +174,7 @@ class Equipment {
 
 //主类  拥于完成出装，战斗。
 public class HYTime2 {
+    //主方法
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //选择英雄并输入信息
@@ -196,16 +200,41 @@ public class HYTime2 {
         String ename = null;
         System.out.println("请选择出装:(1.多兰剑；2.多蓝盾；3.多兰戒)");
         int eq = sc.nextInt();
-        switch (eq) {
+        switch (eq)
+        {
             default:
                 System.out.println("请在给出的装备中选择！");
             case 1:
-
+                Dlsword dlsword = new Dlsword();
+                hp = hp + dlsword.getApPlus();
+                attack = attack + dlsword.getAttackPlus();
+                ap = ap + dlsword.getHpPlus();
+                ename = dlsword.getName();
+                break;
             case 2:
+                Dlring dlring = new Dlring();
+                hp = hp + dlring.getApPlus();
+                attack = attack + dlring.getAttackPlus();
+                ap = ap + dlring.getHpPlus();
+                ename = dlring.getName();
+                break;
+            case 3:
+                Dlshield dlshield = new Dlshield();
+                hp = hp + dlshield.getApPlus();
+                attack = attack + dlshield.getAttackPlus();
+                ap = ap + dlshield.getHpPlus();
+                ename = dlshield.getName();
+                break;
 
         }
         System.out.println("已装备" + ename);
         System.out.println("目前：英雄血量：" + hp + "英雄攻击力：" + attack + "英雄防御力：" + ap);
+    }
+
+    //战斗类
+    public static void fight()
+    {
+
     }
 
 }
@@ -227,7 +256,8 @@ class Archer extends Soldier {
 }
 
 //炮车子类
-class Camion extends Soldier {
+class Camion extends Soldier
+{
     private Camion(String[] args) {
         //实例化对象炮车并录入属性
         Soldier camion = new Soldier("炮车", 100, 40, 15);
@@ -235,19 +265,47 @@ class Camion extends Soldier {
 }
 
 //热武器子类
-class Gun extends Equipment {
-    private Gun(String[] args) {
-        //实例化对象热武器并录入属性
-        Equipment gun = new Equipment("热武器", 300, 50, 40);
-    }
+class Gun extends Equipment
+{
+        public Gun()
+        {
+            //为火炮录入属性
+            super.equipment("火炮",500,50,30);
+        }
 }
 
 //多兰剑子类
-class Dlsword extends Equipment {
-    public Dlsword() {
-        //实例化对象多兰剑并录入属性
-        Equipment dlsword = new Equipment("多兰剑", 0, 25, 0);
+class Dlsword extends Equipment
+{
+    public Dlsword()
+    {
+        //为多兰剑并录入属性
+        super.equipment("多兰剑",50,10,20);
         //特性：每次攻击回复25点血量；
+
+    }
+}
+
+//多兰戒子类
+class Dlring extends Equipment
+{
+    public Dlring()
+    {
+        //为多兰戒并录入属性
+        super.equipment("多兰戒",20,30,0);
+        //特性：每次攻击增加5点攻击；
+
+    }
+}
+
+//多兰盾子类
+class Dlshield extends Equipment
+{
+    public Dlshield()
+    {
+        //为多兰盾并录入属性
+        super.equipment("多兰盾",100,0,30);
+        //特性：每次攻击增加5点防御；
 
     }
 }
